52b84ec3dd8ff41251986d28da31061036807124
Tony <1414927+zfbTony@users.noreply.github.com>
2020-06-13 07:14:22 -0400

    fixed carousel and changed to fade, changed how darkmode is toggled

--- js/main.js
+++ js/main.js
@@ -1,30 +1,45 @@
 var settingStored = false;
+var dark = false;
 if (typeof(Storage) !== "undefined") {
     // enable storing the setting
     if('TorqueDarkMode' in window.localStorage) {
         settingStored = true;
         if(window.localStorage.TorqueDarkMode === 'yes') {
             $("html").addClass("dark-mode");
+            dark = true;
         }
     }
     else {
         // check if they want dark mode explicitly, else set light mode for now
         if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
             $("html").addClass("dark-mode");
+            dark = true;
         }
     }
 } else {
     // query preference and only set dark mode if yes.
     if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
         $("html").addClass("dark-mode");
+        dark = true;
     }
 }
 
 $(document).ready( function() {
-
+    if (dark) {
+        $('#darkmode-toggle').text('Light Mode');
+    }
     if (typeof(Storage) !== "undefined") {
         $("#darkmode-toggle").click(function() { // enable toggle
-            if(settingStored) {
+            if (dark) {
+                window.localStorage.setItem("TorqueDarkMode", "no");
+                $("html").removeClass("dark-mode");
+                $('#darkmode-toggle').text('Light Mode');
+            } else {
+                window.localStorage.setItem("TorqueDarkMode", "yes");
+                $("html").addClass("dark-mode");
+                $('#darkmode-toggle').text('Dark Mode');
+            }
+            /*if(settingStored) {
                 if(window.localStorage.TorqueDarkMode === 'yes') {
                     $("#darkmode-preference").html("Your current preference is set to: <b>dark mode</b>");
                     $("#darkmode-yes").prop("checked", true);
@@ -47,8 +62,9 @@ $(document).ready( function() {
                     $("#darkmode-no").prop("checked", true);
                 }
             }
-            $("#darkmode-modal").modal();
+            $("#darkmode-modal").modal();*/
         });
+        /*
         $("#darkmode-save-button").click(function() {
             if($("#darkmode-yes").prop("checked")) {
                 window.localStorage.setItem("TorqueDarkMode", "yes");
@@ -60,11 +76,21 @@ $(document).ready( function() {
             }
             settingStored = true;
             $("#darkmode-modal").modal('hide');
-        });
+        });*/
     } else {
         // make the toggle just an actual toggle that doesn't save anything
         $("#darkmode-toggle").click(function() {
-            $("html").toggleClass("dark-mode");
+            //$("html").toggleClass("dark-mode");
+            
+            if (dark) {
+                window.localStorage.setItem("TorqueDarkMode", "no");
+                $("html").removeClass("dark-mode");
+                $('#darkmode-toggle').text('Light Mode');
+            } else {
+                window.localStorage.setItem("TorqueDarkMode", "yes");
+                $("html").addClass("dark-mode");
+                $('#darkmode-toggle').text('Dark Mode');
+            }
         });
     }
 
